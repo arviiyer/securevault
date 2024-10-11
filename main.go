55908f0c292d9d4cd023b5d6964a219478fa3173
Arvind Iyer <rbarvind04@gmail.com>
2024-10-10 21:21:05 -0400

    Creating seperate package for encryption logic

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github/arviiyer/ransomware-poc/encryption" // Import the encryption package
+	"github.com/arviiyer/ransomware-poc/encryption" // Import the encryption package
 )
 
 func main() {
