158440a0a19fe8671a9cda90ef504b447c6b93e3
Arvind Iyer <rbarvind04@gmail.com>
2024-10-10 20:57:14 -0400

    Replace original plaintext file with encrypted output

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"crypto/aes"
+  "crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
 	"fmt"
@@ -10,80 +10,92 @@ import (
 )
 
 func getUserInput() string {
-  var input_file string
-  fmt.Print("\nEnter file path: ")
-  fmt.Scan(&input_file)
-  fmt.Println("Entered file path: ", input_file)
-  return input_file
+	var inputFile string
+	fmt.Print("Enter file path: ")
+	fmt.Scanln(&inputFile) // Use Scanln and pass the address of the variable
+	fmt.Println("Entered file path: ", inputFile)
+	return inputFile
 }
 
-func generateAESKey() ([]byte){
-  key := make([]byte, 32) //32 bytes = 256 bits
-  _, err := rand.Read(key)
-  if err!= nil {
-    panic(err.Error())
-  }
-  fmt.Printf("Generated AES Key: %x", key)
-  return key
+func generateAESKey() []byte {
+	key := make([]byte, 32) // 32 bytes = 256 bits
+	_, err := rand.Read(key)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("Generated AES Key: %x\n", key)
+	return key
+}
+
+func createEncryptedFileName(filename string) string {
+	// Remove any existing extension and add ".lock"
+	return filename + ".lock"
 }
 
 func encryptFile() error {
-  key := generateAESKey()
-  input_file := getUserInput()
-  
-  // Create AES cipher block
-  block, err := aes.NewCipher(key)
-  if err != nil {
-    panic(err.Error())
-  }
+	key := generateAESKey()
+	inputFile := getUserInput()
 
-  // Create a new GCM (Galois/Counter Mode) cipher
-  aesgcm, err := cipher.NewGCM(block)
-  if err != nil {
-    panic(err.Error())
-  }
+	// Create AES cipher block
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
 
-  // Generate a nonce (number used once) for GCM encryption
-  nonce := make([]byte, aesgcm.NonceSize())
-  if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-    panic(err.Error())
-  }
+	// Create a new GCM (Galois/Counter Mode) cipher
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
 
-  // Open the input file
-  inFile, err := os.Open(input_file)
-  if err != nil {
-    panic(err.Error())
-  }
-  defer inFile.Close()
+	// Generate a nonce (number used once) for GCM encryption
+	nonce := make([]byte, aesgcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
 
-  // Read file contents
-  file_content, err := io.ReadAll(inFile)
-  if err != nil {
-    panic(err.Error())
-  }
+	// Step 1: Open the input file for reading
+	inFile, err := os.Open(inputFile)
+	if err != nil {
+		return fmt.Errorf("could not open input file: %v", err)
+	}
+	defer inFile.Close()
 
-  //Encrypt the file content
-  ciphertext := aesgcm.Seal(nonce, nonce, file_content, nil)
+	// Read file contents
+	fileContent, err := io.ReadAll(inFile)
+	if err != nil {
+		return fmt.Errorf("could not read input file: %v", err)
+	}
 
-  // Reopen the same file for writing (overwrite mode)
-  outFile, err := os.Create(input_file)
-  if err != nil {
-    panic(err.Error())
-  }
-  defer outFile.Close()
-  
-  // Write the encrypted content back to the input file
-  _, err = outFile.Write(ciphertext)
-  if err != nil {
-    panic(err.Error())
-  }
+	// Step 2: Encrypt the file content
+	ciphertext := aesgcm.Seal(nonce, nonce, fileContent, nil)
 
-  fmt.Println("File encrypted successfully!")
-  return nil
+	// Step 3: Create a new file with the ".enc" extension
+	encFileName := createEncryptedFileName(inputFile)
+	encFile, err := os.Create(encFileName) // Create the new encrypted file
+	if err != nil {
+		return fmt.Errorf("could not create encrypted file: %v", err)
+	}
+	defer encFile.Close()
+
+	// Step 4: Write the encrypted content to the new file
+	_, err = encFile.Write(ciphertext)
+	if err != nil {
+		return fmt.Errorf("could not write encrypted data to file: %v", err)
+	}
+
+	// Step 5: Close and remove the original plaintext file
+	if err := os.Remove(inputFile); err != nil {
+		return fmt.Errorf("could not delete the original file: %v", err)
+	}
+
+	fmt.Println("File encrypted successfully!")
+	return nil
 }
 
 func main() {
-  if err := encryptFile(); err != nil {
-    panic(err.Error())
-  }
+	if err := encryptFile(); err != nil {
+		fmt.Println("Error:", err)
+	}
 }
+
